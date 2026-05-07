d1452d482d19f22472ece8b7d585674a130339ae
Rasmus Lindvall <rasssekonto67@gmail.com>
2026-05-07 13:05:02 +0200

    third commit, start files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Grupp 4 JS3</h1>
+        <p>React Frontend fungerar</p>
+          
+        
         <a
           className="App-link"
           href="https://reactjs.org"
