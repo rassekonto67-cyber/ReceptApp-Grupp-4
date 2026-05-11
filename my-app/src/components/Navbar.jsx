b68910e07a81ad85c63d22e7c103cb2c895bdcf6
Roger <rogerdushime@yahoo.co.uk>
2026-05-11 15:21:24 +0200

    Added navbar and routes

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Hem</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Registrera</Link>
+      <Link to="/profile">Min profil</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
